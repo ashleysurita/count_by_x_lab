9d6abaa28d728774571eee2b5e21128c43ac757e
Ashley <ashleywong678@gmail.com>
2019-10-26 22:21:20 -0400

    Update count_by_x_lab.py

--- count_by_x_lab.py
+++ count_by_x_lab.py
@@ -2,7 +2,4 @@ def count_by(x, n):
     """
     Return a sequence of numbers counting by `x` `n` times.
     """
-    r = []
-    for num in range(x, (n+1)*x, x):
-        r.append(num)
-    return r
+    return range(x, (n+1)*x, x)
